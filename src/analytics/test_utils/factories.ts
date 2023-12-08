c5ff644deeeb9cf5849c0711cf2d76729bc89deb
Samad Yar Khan <smdyarkhan123@gmail.com>
2023-12-09 00:16:33 +0530

    Define factories for tests

---
+++ src/analytics/test_utils/factories.ts
@@ -0,0 +1,52 @@
+import { Review, ReviewEdge } from '@/exapi_sdk/types';
+import { PullRequestGeneratorParams, ReviewGeneratorParams } from './types';
+
+export const getPullRequest = ({
+  repoOwner = 'middlewarehq',
+  repoName = 'unwrapped',
+  createdAt = '2023-02-01T14:04:37Z',
+  mergedAt = '2023-02-03T14:04:37Z',
+  additions = 0,
+  deletions = 0,
+  reviews = []
+}: PullRequestGeneratorParams): any => {
+  return {
+    repository: {
+      name: repoName,
+      owner: {
+        login: repoOwner
+      }
+    },
+    createdAt,
+    mergedAt,
+    additions,
+    deletions,
+    reviews: {
+      edges: reviews.map(getReviewEdge)
+    }
+  };
+};
+
+export const getReview = (args: ReviewGeneratorParams): Review => {
+  const defaults = {
+    reviewerLogin: 'dhruv',
+    createdAt: '2023-02-01T14:04:37Z',
+    state: 'COMMENTED'
+  };
+
+  const { createdAt, reviewerLogin, state } = { ...defaults, ...args };
+
+  return {
+    author: {
+      login: reviewerLogin
+    },
+    createdAt,
+    state
+  };
+};
+
+export const getReviewEdge = (review: Review): ReviewEdge => {
+  return {
+    node: review
+  };
+};
