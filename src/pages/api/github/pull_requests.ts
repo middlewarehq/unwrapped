9b0746b849721f557bdd221b00106c54d7588ac0
Samad Yar Khan <smdyarkhan123@gmail.com>
2023-12-08 15:48:43 +0530

    Add test api to fetch user data

---
+++ src/pages/api/github/pull_requests.ts
@@ -0,0 +1,15 @@
+import { fetchAllPullRequests, fetchUser } from '@/exapi/github';
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const token = req.cookies.ghct || '';
+  const user = await fetchUser(token);
+
+  const pull_request_data = await fetchAllPullRequests(user.login, token);
+  res.status(200).json({
+    pull_request_data
+  });
+}
