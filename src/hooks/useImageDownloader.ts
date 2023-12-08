3e669d46b50e7bef79c2bd062b211b4646bcefac
shivam-bit <shivama5tronot@gmail.com>
2023-12-08 22:48:25 +0530

    refactor: extract single and multiple image function from useImageDownloader

--- src/hooks/useImageDownloader.ts
+++ src/hooks/useImageDownloader.ts
@@ -5,33 +5,32 @@ import { useCallback } from 'react';
 interface DownloadImagesProps {
   images: string | string[];
 }
+const downloadSingleImage = (url: string) => {
+  const fileName = url.substring(url.lastIndexOf('/') + 1);
+  fetch(url)
+    .then((response) => response.blob())
+    .then((blob) => saveAs(blob, fileName))
+    .catch((error) => console.error('Error downloading image:', error));
+};
+
+const downloadMultipleImages = (urls: string[]) => {
+  const zip = new JSZip();
+  const promises = urls.map((url, index) => {
+    const fileName = `image${index + 1}.jpg`;
+
+    return fetch(url)
+      .then((response) => response.blob())
+      .then((blob) => zip.file(fileName, blob));
+  });
+
+  Promise.all(promises)
+    .then(() => zip.generateAsync({ type: 'blob' }))
+    .then((content) => saveAs(content, 'unwrapped-images.zip'))
+    .catch((error) => console.error('Error zipping images:', error));
+};
 
 const useImageDownloader = () => {
   const downloadImages = useCallback(({ images }: DownloadImagesProps) => {
-    const downloadSingleImage = (url: string) => {
-      const fileName = url.substring(url.lastIndexOf('/') + 1);
-      fetch(url)
-        .then((response) => response.blob())
-        .then((blob) => saveAs(blob, fileName))
-        .catch((error) => console.error('Error downloading image:', error));
-    };
-
-    const downloadMultipleImages = (urls: string[]) => {
-      const zip = new JSZip();
-      const promises = urls.map((url, index) => {
-        const fileName = `image${index + 1}.jpg`;
-
-        return fetch(url)
-          .then((response) => response.blob())
-          .then((blob) => zip.file(fileName, blob));
-      });
-
-      Promise.all(promises)
-        .then(() => zip.generateAsync({ type: 'blob' }))
-        .then((content) => saveAs(content, 'unwrapped-images.zip'))
-        .catch((error) => console.error('Error zipping images:', error));
-    };
-
     if (Array.isArray(images)) {
       downloadMultipleImages(images);
     } else {
