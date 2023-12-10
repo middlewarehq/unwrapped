4a83c80878ea3537e2dd8c2ffd0decf37eac1d57
Samad Yar Khan <smdyarkhan123@gmail.com>
2023-12-10 13:57:11 +0530

    Add method for monthwise contribution count getMonthWiseContributionCount

--- src/analytics/contribution_analytics.ts
+++ src/analytics/contribution_analytics.ts
@@ -1,4 +1,5 @@
-import { GithubContributionCalendar } from '@/exapi_sdk/types';
+import { ContributionDay, GithubContributionCalendar } from '@/exapi_sdk/types';
+import { getMonth, parseISO } from 'date-fns';
 
 export const getContributionDaysList = (
   contributionCalendar: GithubContributionCalendar
@@ -6,4 +7,22 @@ export const getContributionDaysList = (
   return contributionCalendar.weeks
     .map((weekData) => weekData.contributionDays)
     .flat();
+};
+
+export const getMonthWiseContributionCount = (
+  contributionCalendar: GithubContributionCalendar
+) => {
+  const contributions: ContributionDay[] =
+    getContributionDaysList(contributionCalendar);
+  let monthNumberToContributionsCountMap: any = {};
+
+  for (let i = 0; i < 12; i++) monthNumberToContributionsCountMap[i] = 0;
+
+  for (let contribution of contributions) {
+    let monthNumber = getMonth(parseISO(contribution.date));
+    monthNumberToContributionsCountMap[monthNumber] +=
+      contribution.contributionCount;
+  }
+
+  return monthNumberToContributionsCountMap;
 };
