d4aa87e219d236432c98e773792c5ed31d03ca33
Samad Yar Khan <smdyarkhan123@gmail.com>
2023-12-12 04:27:07 +0530

    Add API for shared links

---
+++ src/pages/api/shared.ts
@@ -0,0 +1,38 @@
+import chalk from 'chalk';
+import { NextApiRequest, NextApiResponse } from 'next';
+import { fetchSavedCard } from '@/api-helpers/persistance';
+
+const fetchAndDownloadImageBuffer = async (
+  req: NextApiRequest,
+  res: NextApiResponse
+) => {
+  let username = req.query.username as string;
+  let cardName = req.query.cardname as string;
+
+  if (!username) {
+    return res.status(400).json({
+      message: 'Invalid parameters, must pass valid username.'
+    });
+  }
+
+  try {
+    const cachedCardBuffer = await fetchSavedCard(username, cardName);
+
+    if (!cachedCardBuffer) {
+      return res.status(404).json({
+        message: `Image not found for user ${username} and image name ${cardName}`
+      });
+    }
+
+    res.setHeader('Content-Type', 'image/jpeg');
+    res.setHeader('Content-Length', cachedCardBuffer.data.length);
+    res.send(cachedCardBuffer.data);
+
+    console.log(chalk.green('Successfully sent buffer to client'));
+  } catch (error: any) {
+    console.log(chalk.red('Error fetching or sending buffer:'), error);
+    res.status(error.status || 500).json({ message: error.message });
+  }
+};
+
+export default fetchAndDownloadImageBuffer;
