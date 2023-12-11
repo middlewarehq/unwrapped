4b0f21cdc84cf303dec078139ee49944e1f1268d
Jayant Bhawal <bhawal.jayant@gmail.com>
2023-12-11 18:13:37 +0530

    Change where meta tag was added in head

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,20 +6,29 @@ import { AppLoadingStateWrapper } from '@/components/AppLoadingStateWrapper';
 
 import '@/styles/swiper.css';
 import { inter } from '@/styles/fonts';
+import Head from 'next/head';
 
 export default function App({
   Component,
   pageProps: { session, ...pageProps }
 }: AppProps) {
   return (
-    <main className={`${inter} text-white`}>
-      <SessionProvider session={session}>
-        <AppStateProvider>
-          <AppLoadingStateWrapper>
-            <Component {...pageProps} />
-          </AppLoadingStateWrapper>
-        </AppStateProvider>
-      </SessionProvider>
-    </main>
+    <>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+      </Head>
+      <main className={`${inter} text-white`}>
+        <SessionProvider session={session}>
+          <AppStateProvider>
+            <AppLoadingStateWrapper>
+              <Component {...pageProps} />
+            </AppLoadingStateWrapper>
+          </AppStateProvider>
+        </SessionProvider>
+      </main>
+    </>
   );
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,12 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1, shrink-to-fit=no"
-        />
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
