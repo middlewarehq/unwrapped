c106870ef89df8c1314fe0f8dc459ed8c1350d15
Eshaan <76566992+e-for-eshaan@users.noreply.github.com>
2023-12-11 21:23:54 +0530

    fixes the image dimension problem (#71)
    * fixes the image dimension problem
    
    * fixed image url
    
    * fixed margin

--- src/components/templates/IntroCard.tsx
+++ src/components/templates/IntroCard.tsx
@@ -9,7 +9,7 @@ export type IntroCardProps = {
 export const IntroCard: FC<IntroCardProps> = ({ year, username }) => {
   return (
     <RootCard bgColor="red" username="">
-      <div tw="text-yellow-950 flex flex-col w-full h-full p-2 items-center justify-center">
+      <div tw="text-black flex flex-col w-full h-full p-2 items-center justify-center">
         <p>@{username}</p>
         <h1 tw="text-5xl mt-10">{year}</h1>
         <p tw="text-5xl mt-[-20px] mb-10">Unwrapped</p>
