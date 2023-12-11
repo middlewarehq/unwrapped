d09945becd323458245a402f2043886591cafa67
Jayant Bhawal <bhawal.jayant@gmail.com>
2023-12-11 09:57:55 +0530

    Ensure sentry doesn't run locally

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
+const { loadEnvConfig } = require('@next/env');
+const { withSentryConfig } = require('@sentry/nextjs');
+
+loadEnvConfig('.');
+
+console.info('BUILD ENV:', process.env.NEXT_PUBLIC_APP_ENVIRONMENT);
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -11,46 +18,44 @@ const nextConfig = {
   }
 };
 
-module.exports = nextConfig;
+if (process.env.NEXT_PUBLIC_APP_ENVIRONMENT === 'development') {
+  module.exports = nextConfig;
+} else {
+  module.exports = withSentryConfig(
+    module.exports,
+    {
+      // For all available options, see:
+      // https://github.com/getsentry/sentry-webpack-plugin#options
 
-// Injected content via Sentry wizard below
+      // Suppresses source map uploading logs during build
+      silent: true,
+      org: 'middlewarehq',
+      project: 'unwrapped'
+    },
+    {
+      // For all available options, see:
+      // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
 
-const { withSentryConfig } = require('@sentry/nextjs');
+      // Upload a larger set of source maps for prettier stack traces (increases build time)
+      widenClientFileUpload: true,
 
-module.exports = withSentryConfig(
-  module.exports,
-  {
-    // For all available options, see:
-    // https://github.com/getsentry/sentry-webpack-plugin#options
+      // Transpiles SDK to be compatible with IE11 (increases bundle size)
+      transpileClientSDK: true,
 
-    // Suppresses source map uploading logs during build
-    silent: true,
-    org: 'middlewarehq',
-    project: 'unwrapped'
-  },
-  {
-    // For all available options, see:
-    // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+      // Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers (increases server load)
+      tunnelRoute: '/monitoring',
 
-    // Upload a larger set of source maps for prettier stack traces (increases build time)
-    widenClientFileUpload: true,
+      // Hides source maps from generated client bundles
+      hideSourceMaps: true,
 
-    // Transpiles SDK to be compatible with IE11 (increases bundle size)
-    transpileClientSDK: true,
+      // Automatically tree-shake Sentry logger statements to reduce bundle size
+      disableLogger: true,
 
-    // Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers (increases server load)
-    tunnelRoute: '/monitoring',
-
-    // Hides source maps from generated client bundles
-    hideSourceMaps: true,
-
-    // Automatically tree-shake Sentry logger statements to reduce bundle size
-    disableLogger: true,
-
-    // Enables automatic instrumentation of Vercel Cron Monitors.
-    // See the following for more information:
-    // https://docs.sentry.io/product/crons/
-    // https://vercel.com/docs/cron-jobs
-    automaticVercelMonitors: true
-  }
-);
+      // Enables automatic instrumentation of Vercel Cron Monitors.
+      // See the following for more information:
+      // https://docs.sentry.io/product/crons/
+      // https://vercel.com/docs/cron-jobs
+      automaticVercelMonitors: true
+    }
+  );
+}
--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -5,12 +5,14 @@
 
 import * as Sentry from '@sentry/nextjs';
 
-Sentry.init({
-  dsn: process.env.SENTRY_DSN,
+if (process.env.NEXT_PUBLIC_APP_ENVIRONMENT !== 'development') {
+  Sentry.init({
+    dsn: process.env.SENTRY_DSN,
 
-  // Adjust this value in production, or use tracesSampler for greater control
-  tracesSampleRate: 1,
+    // Adjust this value in production, or use tracesSampler for greater control
+    tracesSampleRate: 1,
 
-  // Setting this option to true will print useful information to the console while you're setting up Sentry.
-  debug: false
-});
+    // Setting this option to true will print useful information to the console while you're setting up Sentry.
+    debug: false
+  });
+}
