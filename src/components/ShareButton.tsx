852e17410b666eb18f8d9979a8573122945bc68c
e-for-eshaan <eshaanyadav24@gmail.com>
2023-12-12 01:58:13 +0530

    new colors for share button

--- src/components/ShareButton.tsx
+++ src/components/ShareButton.tsx
@@ -48,12 +48,12 @@ export const ShareButton: React.FC<ShareButtonProps> = ({
       />
 
       {isMenuOpen && (
-        <div className="absolute md:w-96 w-72 md:right-12 right-4 top-12 bg-white rounded-md shadow-lg p-4">
+        <div className="absolute md:w-96 w-72 md:right-12 right-4 top-12 bg-[#11142e] rounded-md shadow-lg p-4">
           <textarea
             placeholder="Check out my GitHub journey of 2023!"
             value={tweetText}
             onChange={(e) => setTweetText(e.target.value)}
-            className="w-full border text-black border-gray-300 rounded-md p-2 mb-2"
+            className="w-full border text-white outline-none rounded-md p-2 mb-2 bg-[#1c2045] border-[#2d3479]"
           />
 
           <div className="w-full flex justify-between">
@@ -61,14 +61,14 @@ export const ShareButton: React.FC<ShareButtonProps> = ({
               <button
                 type="button"
                 onClick={shareToTwitter}
-                className="bg-blue-500 text-white px-2 py-1 rounded hover:bg-blue-600 focus:outline-none focus:ring focus:border-blue-300"
+                className="bg-[#2d3479] text-[#fff] px-2 py-1 rounded hover:bg-[#424db1] focus:outline-none focus:ring focus:border-blue-300"
               >
                 <FaTwitter size={20} />
               </button>
               <button
                 type="button"
                 onClick={callBack}
-                className="bg-blue-500 text-white px-2 py-1 rounded hover:bg-blue-600 focus:outline-none focus:ring focus:border-blue-300"
+                className="bg-[#2d3479] text-white px-2 py-1 rounded hover:bg-[#424db1] focus:outline-none focus:ring focus:border-blue-300"
               >
                 <FaDownload size={18} />
               </button>
@@ -76,7 +76,7 @@ export const ShareButton: React.FC<ShareButtonProps> = ({
             <button
               type="button"
               onClick={toggleMenu}
-              className="bg-red-500 text-white px-2 py-1 rounded hover:bg-red-600 focus:outline-none focus:ring focus:border-red-300"
+              className="bg-[#a23333] text-white px-2 py-1 rounded hover:bg-[#cd4a4a] focus:outline-none focus:ring focus:border-red-300"
             >
               Cancel
             </button>
