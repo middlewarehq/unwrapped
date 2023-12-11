91cb56450c079a6293762c2017f7dff7fff079e2
e-for-eshaan <eshaanyadav24@gmail.com>
2023-12-11 22:05:54 +0530

    added exception to image generator

--- src/api-helpers/vercel-generator.tsx
+++ src/api-helpers/vercel-generator.tsx
@@ -18,27 +18,46 @@ export const createImageUsingVercel = async (
   env: 'node' | 'browser',
   index?: number
 ): Promise<ImageFile> => {
-  const fileName = (index || 0) + 1 + '.png';
-  const fonts = await getFontsForImageGeneration(env);
-  const generatedImage = new ImageResponse(
-    <CardTemplate cardType={cardType} data={data} />,
-    {
-      width: parseInt(CARD_WIDTH) * SCALE_FACTOR,
-      height: parseInt(CARD_HEIGHT) * SCALE_FACTOR,
-      fonts: INTER_FONT_STYLES.map((fontData, index) => ({
-        name: 'Inter',
-        data: fonts[index],
-        style: 'normal',
-        weight: fontData.weight
-      }))
+  try {
+    const fileName = (index || 0) + 1 + '.png';
+    const fonts = await getFontsForImageGeneration(env);
+
+    const generatedImage = new ImageResponse(
+      <CardTemplate cardType={cardType} data={data} />,
+      {
+        width: parseInt(CARD_WIDTH) * SCALE_FACTOR,
+        height: parseInt(CARD_HEIGHT) * SCALE_FACTOR,
+        fonts: INTER_FONT_STYLES.map((fontData, index) => ({
+          name: 'Inter',
+          data: fonts[index],
+          style: 'normal',
+          weight: fontData.weight
+        }))
+      }
+    );
+
+    let imageCopy = generatedImage.clone();
+    let imageArrayBuffer;
+
+    try {
+      imageArrayBuffer = await generatedImage.arrayBuffer();
+    } catch (arrayBufferError) {
+      console.error(
+        'Error converting image to array buffer:',
+        arrayBufferError
+      );
+      throw new Error('Image array buffer conversion failed');
     }
-  );
-  let imageCopy = generatedImage.clone();
-  const imageArrayBuffer = await generatedImage.arrayBuffer();
-  const imageBuffer = arrayBufferToBuffer(imageArrayBuffer);
-  return {
-    data: imageBuffer,
-    fileName,
-    image: imageCopy
-  };
+
+    const imageBuffer = arrayBufferToBuffer(imageArrayBuffer);
+
+    return {
+      data: imageBuffer,
+      fileName,
+      image: imageCopy
+    };
+  } catch (error) {
+    console.error('Error in createImageUsingVercel:', error);
+    throw new Error('Image creation failed');
+  }
 };
--- src/pages/api/image-gen/index.ts
+++ src/pages/api/image-gen/index.ts
@@ -6,6 +6,7 @@ import { DEV } from '../../../constants/general';
 import { updatedGhData } from '@/mocks/github';
 import { getDataFromGithubResponse } from '@/api-helpers/card-data-adapter';
 import { CardTypes, sequence } from '../../../types/cards';
+import { ImageFile } from '@/types/images';
 
 export const fetchData = async (): Promise<GitHubDataResponse> => {
   if (process.env.NEXT_PUBLIC_APP_ENVIRONMENT === DEV) {
@@ -24,26 +25,31 @@ export const fetchData = async (): Promise<GitHubDataResponse> => {
 export const generateImages = async (
   data: GitHubDataResponse,
   customSequence?: CardTypes[]
-) => {
-  console.log(chalk.yellow('Generating images...'));
+): Promise<ImageFile[]> => {
+  try {
+    console.log(chalk.yellow('Generating images...'));
 
-  const cardSequence = customSequence || sequence;
+    const cardSequence = customSequence || sequence;
 
-  const adaptedData = getDataFromGithubResponse(data);
-  const cardsToBeGenerated = cardSequence.filter((card) =>
-    Boolean(adaptedData[card])
-  );
+    const adaptedData = getDataFromGithubResponse(data);
+    const cardsToBeGenerated = cardSequence.filter((card) =>
+      Boolean(adaptedData[card])
+    );
 
-  const imageFileBuffers = await Promise.all(
-    cardsToBeGenerated.map((cardName, index) =>
-      createImageUsingVercel(
-        { ...adaptedData[cardName], username: data.user.login },
-        cardName,
-        'node',
-        index
+    const imageFileBuffers = await Promise.all(
+      cardsToBeGenerated.map((cardName, index) =>
+        createImageUsingVercel(
+          { ...adaptedData[cardName], username: data.user.login },
+          cardName,
+          'node',
+          index
+        )
       )
-    )
-  );
+    );
 
-  return imageFileBuffers;
+    return imageFileBuffers;
+  } catch (error) {
+    console.error('Error in generateImages:', error);
+    throw new Error('Image generation failed');
+  }
 };
