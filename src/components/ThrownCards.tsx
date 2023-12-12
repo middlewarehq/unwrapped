593418c7b00785ec4e29d233c7b6978d6c3191ef
Jayant Bhawal <bhawal.jayant@gmail.com>
2023-12-12 16:02:23 +0530

    Add click handler to go to popdevs on thrown cards

--- src/components/ThrownCards.tsx
+++ src/components/ThrownCards.tsx
@@ -2,22 +2,30 @@ import linusVillage from '@/assets/popdevs/linus/village.png';
 import danOss from '@/assets/popdevs/dan/oss.png';
 import adamDrake from '@/assets/popdevs/tailwind/drake.png';
 
+const goToPopDevs = () =>
+  document
+    .getElementById('popular-devs')
+    ?.scrollIntoView({ behavior: 'smooth' });
+
 export const ThrownCards = () => (
   <div className="absolute right-0 max-lg:-right-40 bottom-0 w-60 h-60 max-[700px]:hidden">
     <img
       src={danOss.src}
       alt=""
-      className="rounded-md absolute bottom-0 right-72 md:scale-100 xl:scale-125 -rotate-[30deg]"
+      className="rounded-md absolute bottom-0 right-72 md:scale-100 xl:scale-125 -rotate-[30deg] cursor-pointer"
+      onClick={goToPopDevs}
     />
     <img
       src={linusVillage.src}
       alt=""
-      className="rounded-md absolute bottom-0 right-36 md:scale-90 xl:scale-110 -rotate-[15deg]"
+      className="rounded-md absolute bottom-0 right-36 md:scale-90 xl:scale-110 -rotate-[15deg] cursor-pointer"
+      onClick={goToPopDevs}
     />
     <img
       src={adamDrake.src}
       alt=""
-      className="rounded-md absolute bottom-0 right-0 md:scale-90 xl:scale-100 -rotate-6 max-lg:hidden"
+      className="rounded-md absolute bottom-0 right-0 md:scale-90 xl:scale-100 -rotate-6 max-lg:hidden cursor-pointer"
+      onClick={goToPopDevs}
     />
   </div>
 );
