83d7dca88188ae185e10833e7a30cd84ada058e5
Jayant Bhawal <bhawal.jayant@gmail.com>
2023-12-17 18:57:34 +0530

    pm2 restart process without deleting it

--- scripts/server-init.sh
+++ scripts/server-init.sh
@@ -10,8 +10,9 @@ install_yarn_cmd_if_not_exists pm2
 # install_if_missing_vector
 
 set +e
-pm2 delete MHQ_HTTP_SERVER
+pm2 stop MHQ_HTTP_SERVER || true
 set -e
 
 NEXT_MANUAL_SIG_HANDLE=true
 pm2 start "yarn start" --name MHQ_HTTP_SERVER --time --max-memory-restart 2G
+pm2 save
